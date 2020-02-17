df9444fd08255c9eebf45f0fce500e954834175d
Kenny Cruz <kac.garcia@yavirac.edu.ec>
2020-02-17 12:46:59 -0500

    subida back

---
+++ src/test/java/ec/edu/espe/Tutorias/TutoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.Tutorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
